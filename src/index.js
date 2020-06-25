e9d0c1b0f6081e6900357212e58319e18316f021
Marcin Polak <mapoart@gmail.com>
2020-06-25 15:34:27 +0200

    new version, works with nxsAs

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-// Nexss PROGRAMMER 2.0.0 - NodeJS
-// Default template for JSON Data
-// STDIN
-process.stdin.on("data", function(NexssStdin) {
-  var cuid = require("cuid");
-  let NexssStdout;
-  try {
-    NexssStdout = JSON.parse(NexssStdin.toString());
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-  // Modify data
-  //NexssStdout.NodeJSOutput = `Hello from NodeJS! ${process.version}`;
-  NexssStdout.cuid = cuid();
-  // STDOUT
-  process.stdout.write(JSON.stringify(NexssStdout));
-});
+const {
+  nxsError,
+} = require(`${process.env.NEXSS_PACKAGES_PATH}/Nexss/Lib/NexssLog.js`);
 
-process.stdin.on("end", function() {
-  //On Windows below is not needed.
-  process.exit(0);
-});
+const NexssIn = require(`${process.env.NEXSS_PACKAGES_PATH}/Nexss/Lib/NexssIn.js`);
+let NexssStdout = NexssIn();
+
+var cuid = require("cuid");
+NexssStdout[NexssStdout.resultField_1] = cuid();
+
+delete NexssStdout.nxsIn;
+delete NexssStdout.resultField_1;
+process.stdout.write(JSON.stringify(NexssStdout));
